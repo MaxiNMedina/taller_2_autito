db3db276a116fcc41355086ac2e5027300a3e4a7
MaxiNMedina <maaxii.cb@hotmail.com>
2019-10-08 19:23:22 -0300

    funciona

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,23 @@ FROM alpine:3.10
 RUN apk add --no-cache python3-dev \
     && pip3 install --upgrade pip
 
-RUN pip install flask-bootstrap 
+RUN pip install flask-bootstrap
+
+RUN set -e; \
+        apk add --no-cache --virtual .build-deps \
+                gcc \
+                libc-dev \
+                linux-headers \
+                mariadb-dev \
+                python3-dev \
+                postgresql-dev \
+        ;
+RUN pip install flask-mysqldb 
 
 WORKDIR /app
 COPY . /app
 
 RUN pip3 --no-cache-dir install -r requirements.txt
+
 
 CMD ["python3", "src/app.py"]
--- src/app.py
+++ src/app.py
@@ -2,33 +2,49 @@ from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap
 import requests
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 Bootstrap(app)
 
+'''app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'maximedina'
+app.config['MYSQL_PASSWORD'] = 'maximedina'
+app.config['MYSQL_DB'] = 'autito'
+mysql = MySQL(app)'''
+
 @app.route('/', methods= ['GET', 'POST'])
 def index():
+    '''cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM usuarios')
+    data = cur.fetchall()
+    print(data)'''
     return render_template('index.html') 
 
 @app.route('/adelante')
 def adelante():
-    return requests.get('http://192.168.4.1/1')
+    requests.get('http://192.168.4.1/1')
+    return render_template('index.html') 
 
 @app.route('/atras')
 def atras():
-    return requests.get('http://192.168.4.1/2')
+   requests.get('http://192.168.4.1/2')
+   return render_template('index.html') 
 
 @app.route('/izquierda')
 def izquierda():
-    return requests.get('http://192.168.4.1/3')
+    requests.get('http://192.168.4.1/3')
+    return render_template('index.html') 
 
 @app.route('/derecha')
 def derecha():
-    return requests.get('http://192.168.4.1/4')
+    requests.get('http://192.168.4.1/4')
+    return render_template('index.html') 
 
 @app.route('/parar')
 def parar():
-    return requests.get('http://192.168.4.1/5')
+    requests.get('http://192.168.4.1/5')
+    return render_template('index.html') 
 
 @app.route('/velocidad')
 def action_form(vel=None):
